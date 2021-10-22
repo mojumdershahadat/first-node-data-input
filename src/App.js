682bc6637233447591ad9ad83e563b78891b4775
unknown <mojumdershahadat32@gmail.com>
2021-10-22 18:21:22 +0600

    from node data input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (e) => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = { name: name, email: email };
+    // send data to the server
+    fetch("http://localhost:3000/users", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const addedUser = data;
+        const newUsers = [...users, addedUser];
+        setUsers(newUsers);
+      });
+    nameRef.current.value = "";
+    emailRef.current.value = "";
+    e.preventDefault();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>users: {users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" ref={nameRef} name="" placeholder="name" id="" />
+        <input
+          type="email"
+          name=""
+          placeholder="your email"
+          ref={emailRef}
+          id=""
+        />
+        <input type="submit" value="submit" />
+      </form>
+      {users.map((user) => (
+        <li>{user.name}</li>
+      ))}
     </div>
   );
 }
